502c93f22e4c18d0ca60689efcea2e982f01c747
Alisson-Oliveira <alisskn.olisskn@gmail.com>
2020-01-29 14:27:31 -0300

    Second module [ Webpack Server ]

---
+++ Curso Javascript ES6/Webpack Server/main.js
@@ -0,0 +1,10 @@
+import Usuario, { idade } from './functions';
+import Usuario, { idade as IdadeUsuario } from './functions';
+
+Usuario.info();
+console.log(idade);
+console.log(IdadeUsuario);
+
+// É necessario baixar e instalar o webpack na pasta do projeto. nesta pasta não está instalado.
+// Qualquer duvida, só olhar o arquivo "main.js" da pasta "src" do projeto Curso-ES6.
+
