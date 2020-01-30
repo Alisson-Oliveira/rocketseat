6ac056e2275ed0d80ca3032ebb413d03b5ee39a1
Alisson-Oliveira <alisskn.olisskn@gmail.com>
2020-01-30 12:04:52 -0300

    Curso ReactJS [Iniciando]

---
+++ Curso ReactJS/HuntWeb/src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./styles.css";
+
+const Header = () => <header id="main-header"> JS Hunt </header>;
+
+export default Header; 
---
+++ Curso ReactJS/HuntWeb/src/pages/main/index.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+
+import './styles.css';
+
+export default class Main extends Component {
+  state = {
+    products: [],
+    productInfo: {},
+    page: 1,
+  }
+  
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+    
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ products: response.data.docs, productInfo, page });
+  };
+  
+  prevPage = () => {
+    const { page } = this.state;
+
+    if (page === 1) return;
+    
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+    
+  }
+
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+
+    if (page === productInfo.pages) return;
+
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
+  }
+
+  render() {
+    const { products, page, productInfo } = this.state;
+
+    return (
+      <div className="product-list">
+        { 
+          products.map(products => (
+            <article key={products._id}>
+              <strong> { products.title } </strong>
+              <p> { products.description } </p>
+              <a href=""> Acessar </a>
+            </article>
+          ))
+        }
+        <div className="actions">
+          <button disabled={page===1} onClick={this.prevPage}>
+            Anterior
+          </button>
+          <button disabled={page===productInfo.pages} onClick={this.nextPage}>
+            Próximo
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
