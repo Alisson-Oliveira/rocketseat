523a1265ee974f643e7211722dff152384b758ac
Alisson-Oliveira <alisskn.olisskn@gmail.com>
2021-01-08 21:46:30 -0300

    Update event next-level-week-2.

---
+++ Eventos/next-level-week-2/README.md
@@ -0,0 +1,19 @@
+# Proffy
+
+Aplicação completa com mobile, web e server.
+
+- Mobile feito com expo.
+- Web feito com react.
+- Server feito com node.
+
+O evento Next Level Week 2 que foi realizado no periodo de 03 à 07 de Agosto de 2020.
+
+Desenvolvido pela @Rocketseat.
+
+# Resumo da aplicação 
+
+A aplicação é direcionada para professores e alunos.
+
+Os professores podem cadastrar quais aulas querem ministrar e em quais dias e horários da semana.
+
+Os alunos podem entrar em contato com os professores.
---
+++ Eventos/next-level-week-2/web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import api from '../../services/api';
+
+import './styles.css';
+
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+export interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function handleCreateNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src={teacher.avatar} alt="Alisson Oliveira" />
+        <div>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
+        </div>
+      </header>
+      <p>{teacher.bio}</p>
+      <footer>
+        <p>
+          Preço/Hora
+          <strong>R$ {teacher.cost}</strong>
+        </p>
+        <a 
+          target="_blank"
+          onClick={handleCreateNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </a>
+      </footer>
+    </article>
+  )
+}
+
+export default TeacherItem;
